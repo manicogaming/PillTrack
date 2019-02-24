a81a7e032c2208913308da06fe036da3d5cfa82a
manico <manicogaming@gmail.com>
2019-02-24 02:37:13 +0000

    Popup para mudar a palavra-passe criado

--- app/src/main/java/com/pap/diogo/pilltrack/MainActivity.java
+++ app/src/main/java/com/pap/diogo/pilltrack/MainActivity.java
@@ -3,6 +3,7 @@ package com.pap.diogo.pilltrack;
 import android.accounts.Account;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -11,11 +12,16 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -75,6 +81,38 @@ public class MainActivity extends AppCompatActivity {
 
         add_button.setVisibility(View.VISIBLE);
 
+        Button accountChangePass = findViewById(R.id.AccountChangePass);
+
+        accountChangePass.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // inflate the layout of the popup window
+                LayoutInflater inflater = (LayoutInflater)
+                        getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.popup_changepw, null);
+
+                // create the popup window
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    popupWindow.setElevation(20);
+                }
+
+                // show the popup window
+                popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+
+                // dismiss the popup window when touched
+                popupView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
+            }
+        });
     }
 
 
@@ -118,6 +156,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            Intent VerifyLogin = new Intent(MainActivity.this, Launcher.class);
+            VerifyLogin.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(VerifyLogin);
+        }
 
         pills_layout = findViewById(R.id.pills_layout);
         appoint_layout = findViewById(R.id.appoint_layout);
