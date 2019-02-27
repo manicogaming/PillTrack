5f3c726fecdbc7351fbb2201cb1fa40bf17503d6
manico <manicogaming@gmail.com>
2019-02-27 22:31:27 +0000

    Pequenas Mudanças

--- app/src/main/java/com/pap/diogo/pilltrack/AccountFragment.java
+++ app/src/main/java/com/pap/diogo/pilltrack/AccountFragment.java
@@ -4,14 +4,105 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class AccountFragment extends Fragment {
+    private View view;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_account, container, false);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        /*RecyclerView recyclerView = findViewById(R.id.accountlist);*/
+
+        /*LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);*/
+        /*recyclerView.setLayoutManager(linearLayoutManager);*/
+        /*recyclerView.setHasFixedSize(true);*/
+
+        Query query = FirebaseDatabase.getInstance()
+                .getReference()
+                .child("Users");
+
+        FirebaseRecyclerOptions<Account> options =
+                new FirebaseRecyclerOptions.Builder<Account>()
+                        .setQuery(query, new SnapshotParser<Account>() {
+                            @NonNull
+                            @Override
+                            public Account parseSnapshot(@NonNull DataSnapshot snapshot) {
+                                return new Account(snapshot.child("name").getValue().toString(),
+                                        snapshot.child("idade").getValue().toString());
+                            }
+                        })
+                        .build();
+
+        FirebaseRecyclerAdapter adapter = new FirebaseRecyclerAdapter<Account, ViewHolder>(options) {
+            @Override
+            public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.account, parent, false);
+
+                return new ViewHolder(view);
+            }
+
+
+            @Override
+            protected void onBindViewHolder(ViewHolder holder, final int position, Account model) {
+                holder.setTxtTitle(model.getName());
+                holder.setTxtDesc(model.getIdade());
+
+                holder.root.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+            }
+
+        };
+        /*recyclerView.setAdapter(adapter);*/
+        adapter.startListening();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public RelativeLayout root;
+        public TextView txtTitle;
+        public TextView txtDesc;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            root = itemView.findViewById(R.id.account);
+            txtTitle = itemView.findViewById(R.id.AccountName0);
+            txtDesc = itemView.findViewById(R.id.AccountAge0);
+        }
+
+        public void setTxtTitle(String string) {
+            txtTitle.setText(string);
+        }
+
+
+        public void setTxtDesc(String string) {
+            txtDesc.setText(string);
+        }
+    }
 }
+
--- app/src/main/java/com/pap/diogo/pilltrack/MainActivity.java
+++ app/src/main/java/com/pap/diogo/pilltrack/MainActivity.java
@@ -31,10 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
     private RelativeLayout pills_layout, appoint_layout, add_pills_layout, add_appoints_layout, account_layout, add_button;
     private TextView AccountName0, AccountAge0;
-    private RecyclerView recyclerView;
-    private LinearLayoutManager linearLayoutManager;
-    private FirebaseRecyclerAdapter adapter;
-    private View view;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -75,85 +71,18 @@ public class MainActivity extends AppCompatActivity {
             startActivity(VerifyLogin);
         }
 
-        /*recyclerView = findViewById(R.id.accountlist);
-
-        linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setHasFixedSize(true);*/
-
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
 
-    /*public class ViewHolder extends RecyclerView.ViewHolder {
-        public RelativeLayout root;
-        public TextView txtTitle;
-        public TextView txtDesc;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            root = itemView.findViewById(R.id.account);
-            txtTitle = itemView.findViewById(R.id.AccountName0);
-            txtDesc = itemView.findViewById(R.id.AccountAge0);
-        }
-
-        public void setTxtTitle(String string) {
-            txtTitle.setText(string);
-        }
-
-
-        public void setTxtDesc(String string) {
-            txtDesc.setText(string);
-        }
-    }*/
+    /**/
 
     @Override
     protected void onStart() {
         super.onStart();
-        /*Query query = FirebaseDatabase.getInstance()
-                .getReference()
-                .child("Users");
-
-        FirebaseRecyclerOptions<Account> options =
-                new FirebaseRecyclerOptions.Builder<Account>()
-                        .setQuery(query, new SnapshotParser<Account>() {
-                            @NonNull
-                            @Override
-                            public Account parseSnapshot(@NonNull DataSnapshot snapshot) {
-                                return new Account(snapshot.child("name").getValue().toString(),
-                                        snapshot.child("idade").getValue().toString());
-                            }
-                        })
-                        .build();
-
-        adapter = new FirebaseRecyclerAdapter<Account, ViewHolder>(options) {
-            @Override
-            public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.account, parent, false);
-
-                return new ViewHolder(view);
-            }
-
-
-            @Override
-            protected void onBindViewHolder(ViewHolder holder, final int position, Account model) {
-                holder.setTxtTitle(model.getName());
-                holder.setTxtDesc(model.getIdade());
-
-                holder.root.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(MainActivity.this, String.valueOf(position), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-
-        };
-        recyclerView.setAdapter(adapter);
-        adapter.startListening();*/
+        /**/
     }
 
     @Override
