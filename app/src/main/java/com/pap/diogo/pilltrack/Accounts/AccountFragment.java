a6b3815a5d0c2be2e60c9f801f399e7893e62c00
manico <manicogaming@gmail.com>
2019-06-02 15:54:09 +0100

    Limpeza de código

--- app/src/main/java/com/pap/diogo/pilltrack/Accounts/AccountFragment.java
+++ app/src/main/java/com/pap/diogo/pilltrack/Accounts/AccountFragment.java
@@ -1,15 +1,10 @@
 package com.pap.diogo.pilltrack.Accounts;
 
-import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,21 +15,15 @@ import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.core.view.Change;
 import com.pap.diogo.pilltrack.Launcher;
 import com.pap.diogo.pilltrack.MainActivity;
 import com.pap.diogo.pilltrack.R;
-
-import org.w3c.dom.Text;
 
 public class AccountFragment extends Fragment {
     private ArrayAdapter<String> adapter;
